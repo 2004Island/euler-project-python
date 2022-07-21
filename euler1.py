fbe3c7d68f1efc90f040605360c744c981037ebb
Varun Sreedharan <varunsreedharan22@gmail.com>
2022-07-20 22:00:30 -0700

    first commit

---
+++ euler1.py
@@ -0,0 +1,12 @@
+###EULER 1
+###Varun Sreedharan 07/20/22
+
+numbers = []
+
+for i in range (0,1000):
+    if i%3 == 0 or i%5 == 0:
+        numbers.append(i)
+    else:
+        continue
+
+print(sum(numbers))
---
+++ euler2.py
@@ -0,0 +1,16 @@
+###EULER 2
+###Varun Sreedharan 07/20/22
+
+fibnums = [0, 1, 2]
+evenfibnums = [0, 2]
+tempfib = 0
+while tempfib < 4000000:
+    tempfib = fibnums[-1] + fibnums[-2]
+    fibnums.append(tempfib)
+    
+    if tempfib%2 == 0:
+        evenfibnums.append(tempfib)
+    else:
+        continue
+    
+print(sum(evenfibnums))
